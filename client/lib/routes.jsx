52694737622131c8960afe840e021fb059bf1141
Servio Mora <servio.mora19@gmail.com>
2016-07-05 22:50:54 -0400

    multiple routes and views

---
+++ client/lib/routes.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {mount} from 'react-mounter';
+
+import {MainLayout} from '../layouts/main-layout.jsx';
+import Home from '../views/Home.jsx';
+import {Me} from '../views/Me.jsx';
+
+FlowRouter.route('/', {
+  action() {
+    mount(MainLayout, {
+      content: (<Home />)
+    });
+  }
+});
+
+FlowRouter.route('/me', {
+  action() {
+    mount(MainLayout ,{
+      content: (<Me />)
+    })
+  }
+})
